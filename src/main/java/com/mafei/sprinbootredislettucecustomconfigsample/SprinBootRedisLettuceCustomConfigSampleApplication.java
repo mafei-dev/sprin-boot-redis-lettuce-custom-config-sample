bd5139bed9341a4489c47c6b29af652651ab573a
mafei <mafei.dev@gmail.com>
2022-02-13 16:28:48 +0530

    Initial commit

---
+++ src/main/java/com/mafei/sprinbootredislettucecustomconfigsample/SprinBootRedisLettuceCustomConfigSampleApplication.java
@@ -0,0 +1,13 @@
+package com.mafei.sprinbootredislettucecustomconfigsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootRedisLettuceCustomConfigSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinBootRedisLettuceCustomConfigSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mafei/sprinbootredislettucecustomconfigsample/SprinBootRedisLettuceCustomConfigSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mafei.sprinbootredislettucecustomconfigsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinBootRedisLettuceCustomConfigSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
